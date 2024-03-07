d24f8ba448dd1e3ddc14772c789a5d3a1e0672ab
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2024-03-08 07:33:35 +0800

    innitial commit

---
+++ api/app.py
@@ -0,0 +1,37 @@
+import flask
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+###############################
+############ MAIN #############
+###############################
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about-us')
+def about():
+    return render_template('about.html')
+
+###############################
+############ ERROR ############
+###############################
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
+
+###############################
+######## RUN TIME CODE ########
+###############################
+if __name__ == "__main__":
+    app.run(debug=True)
